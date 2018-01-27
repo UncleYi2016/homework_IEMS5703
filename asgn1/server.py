ed461d99cabdbd50845d92901da875a05245b967
UncleYi <yizhaopu2008@163.com>
2018-01-27 15:07:14 +0800

    +close()

--- asgn1/server.py
+++ asgn1/server.py
@@ -8,3 +8,9 @@ END_STRING = '--- THE END ---'
 # Create and initialize server_socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('', SERVER_PORT))
+
+threads = []
+while True:
+    (client_socket, client_address) = server_socket.accept()
+    print(client_socket)
+    client_socket.close()
