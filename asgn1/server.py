813214282fcf6e150ff4a983dfa9651d043fcf99
UncleYi <yizhaopu2008@163.com>
2018-01-27 14:51:11 +0800

    add server.py & client.py

---
+++ asgn1/server.py
@@ -0,0 +1,10 @@
+import socket
+import json
+
+SERVER_PORT = 55703
+BUFFER_SIZE = 2048
+END_STRING = '--- THE END ---'
+
+# Create and initialize server_socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind('', SERVER_PORT)
