3da317037ecacafa53779ea2ebe4ec1afd8c2c2e
UncleYi <yizhaopu2008@163.com>
2018-02-05 21:24:58 +0800

    ok

--- asgn1/client.py
+++ asgn1/client.py
@@ -1,10 +1,13 @@
 import socket
 import json
+from data_package import data_package
 
 SERVER_PORT = 55703
 BUFFER_SIZE = 2048
 END_STRING = '--- THE END ---'.encode('utf-8')
 
+dp = data_package('test')
+print(dp)
 # Create and initialize client_socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', SERVER_PORT))
---
+++ asgn1/data_package.py
@@ -0,0 +1,11 @@
+class data_package:
+    data = ''
+
+    def __init__(self):
+        pass
+    
+    def __init__(self, data):
+        self.data = data
+    
+    def __str__(self):
+        return data
