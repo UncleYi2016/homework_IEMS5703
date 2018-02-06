827c92e4722630a2ee52e9224bd2c01346281892
UncleYi <yizhaopu2008@163.com>
2018-02-06 17:44:27 +0800

    o

--- asgn1/client.py
+++ asgn1/client.py
@@ -7,9 +7,6 @@ BUFFER_SIZE = 2048
 END_STRING = '[END]'
 
 logging.basicConfig(level = logging.DEBUG)
-
-dp = data_package('test')
-print(dp)
 # Create and initialize client_socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', SERVER_PORT))
