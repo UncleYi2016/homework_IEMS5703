0d663b69fd7747d885365175f0bc7f05748ad6e4
UncleYi <yizhaopu2008@163.com>
2018-03-18 11:59:18 +0800

    sdf

--- asgn3/server.py
+++ asgn3/server.py
@@ -18,27 +18,11 @@ def load_database():
             length = len(row)
             for i in range(length):
                 movie[keys[i]] = row[i]
+                if keys[i] == 'Rank':
+                    movie['id'] = int(row[i]) - 1
             MOVIES.append(movie)
-    # dic_key = file.readline()
-    # dic_key = dic_key.strip().split(',')  # The dictionary keys
-    # keys = []                           # Store key list into array
-    # for key in dic_key:
-    #     keys.append(key)
-    # print(len(keys))
-    # for line in file:
-    #     movie_element = line.split(',')
-    #     for element in movie_element:
-    #         if element.
-    #     movie = {}
-    #     length = len(movie_element)
-    #     print(length)
-    #     print(json.dumps(movie_element))
-    #     for i in range(length):
-    #         print(i)
-    #         movie[keys[i]] = movie_element[i]
-    # movie['comment'] = []
 
 if __name__ == '__main__':
     load_database()
-    json_str = json.dumps(MOVIES[0])
+    json_str = json.dumps(MOVIES[2])
     print(json_str)
