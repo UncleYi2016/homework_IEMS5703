805e6bfdc2e9a235321d621db543d2c1327c9e95
UncleYi <yizhaopu2008@163.com>
2018-03-18 11:53:52 +0800

    dsf

--- asgn3/server.py
+++ asgn3/server.py
@@ -6,10 +6,18 @@ MOVIES = []
 def load_database():
     file = open(DATABASE_PATH, 'r', encoding='utf-8')
     s = csv.reader(file)
-    print(s[0])
-    # for row in s:
-    #     for key in row:
-    #         keys.append()
+    first_row = True
+    for row in s:
+        if first_row:
+            for key in row:
+                keys.append(key)
+            first_row = False
+        else:
+            movie = {}
+            length = len(row)
+            for i in range(length):
+                movie[keys[i]] = row[i]
+            MOVIES.append(movie)
     # dic_key = file.readline()
     # dic_key = dic_key.strip().split(',')  # The dictionary keys
     # keys = []                           # Store key list into array
