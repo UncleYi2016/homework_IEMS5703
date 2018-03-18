f95929d0504c55f9247a510efdc5b3b5f1ea7fb8
UncleYi <yizhaopu2008@163.com>
2018-03-18 11:22:45 +0800

    sdf

--- asgn3/server.py
+++ asgn3/server.py
@@ -1,26 +1,31 @@
 import json
-
+import csv
 DATABASE_PATH = 'imdb_top1000.csv'
 MOVIES = []
 
 def load_database():
     file = open(DATABASE_PATH, 'r', encoding='utf-8')
-    dic_key = file.readline()
-    dic_key = dic_key.strip().split(',')  # The dictionary keys
-    keys = []                           # Store key list into array
-    for key in dic_key:
-        keys.append(key)
-    print(len(keys))
-    for line in file:
-        movie_element = line.split(',')
-        movie = {}
-        length = len(movie_element)
-        print(length)
-        print(json.dumps(movie_element))
-        for i in range(length):
-            print(i)
-            movie[keys[i]] = movie_element[i]
-    movie['comment'] = []
+    s = csv.reader(file)
+    print(s)
+    print(json.dumps(s))
+    # dic_key = file.readline()
+    # dic_key = dic_key.strip().split(',')  # The dictionary keys
+    # keys = []                           # Store key list into array
+    # for key in dic_key:
+    #     keys.append(key)
+    # print(len(keys))
+    # for line in file:
+    #     movie_element = line.split(',')
+    #     for element in movie_element:
+    #         if element.
+    #     movie = {}
+    #     length = len(movie_element)
+    #     print(length)
+    #     print(json.dumps(movie_element))
+    #     for i in range(length):
+    #         print(i)
+    #         movie[keys[i]] = movie_element[i]
+    # movie['comment'] = []
 
 if __name__ == '__main__':
     load_database()
