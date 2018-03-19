1b054ef1550a1afd985430bf9edf15ec9f0bc42f
UncleYi <yizhaopu2008@163.com>
2018-03-19 20:09:48 +0800

    sdf

--- asgn3/server.py
+++ asgn3/server.py
@@ -123,7 +123,7 @@ def search():
         movie_element.pop('Votes')
         movie_element.pop('comments')
         movie_query.append(movie_element)
-    movie_query.sort(lambda x,y: cmp(x[sortby], y[sortby]), reversed = reverse)
+    movie_query.sort(lambda x,y: cmp(x[sortby], y[sortby]), reverse = reverse)
     movie_query_result = movie_query[0:9]
     return json.dumps(movie_query_result)
 
