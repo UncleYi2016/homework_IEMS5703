618bb89ba46b210ff56c86e8c77b9b131826712d
UncleYi <yizhaopu2008@163.com>
2018-03-21 12:19:38 +0800

    sadf

--- asgn3/client.py
+++ asgn3/client.py
@@ -34,6 +34,7 @@ if __name__ == '__main__':
         print('What is your comment? <User inputs his/her comment here and press enter>')
         comment = input()
         params = urllib.parse.urlencode({'user_name': user_name, 'movie_id': movie_id, 'comment': comment})
+        params = params.encode('ascii')
         url = 'http://' + addr + ':' + str(port) + '/comment'
         with urllib.request.urlopen(url, params) as f:
             json_str = f.read().decode('utf-8')
