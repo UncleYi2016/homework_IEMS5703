e9b0a8ae015e874255dc10554741382cb2d5171e
UncleYi <yizhaopu2008@163.com>
2018-03-21 12:07:31 +0800

    sdf

---
+++ asgn3/client.py
@@ -0,0 +1,20 @@
+import json
+import sys
+import urllib
+
+
+if __name__ == '__main__':
+    addr = sys.argv[1]
+    port = int(sys.argv[2])
+    function = sys.argv[3]
+
+    if function == 'search':
+        query = sys.argv[4]
+        attribute = sys.argv[5]
+        sortby = sys.argv[6]
+        order = sys.argv[7]
+        params = urllib.parse.urlencode({'query': query, 'attribute': attribute, 'sortby': sortby, 'order': order})
+        url = 'http://' + addr + ':' + port + '/search?%s' % params
+        print(url)
+        with urllib.request.urlopen(url) as f:
+            print(f.read.decode('utf-8'))
