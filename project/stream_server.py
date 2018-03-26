d38a69f7ad454ebfdf0ae0dfc2053ac145512355
UncleYi <yizhaopu2008@163.com>
2018-03-26 16:32:28 +0800

    sadf

--- project/stream_server.py
+++ project/stream_server.py
@@ -2,11 +2,11 @@ from flask import Response
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/large.csv')
+@app.route('/test')
 def generate_large_csv():
     def generate():
         count = 0
         while True:
             count += 1
             yield str(count) + '\n'
-    return Response(generate(), mimetype='text/csv')
+    return Response(generate())
