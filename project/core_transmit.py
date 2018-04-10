de678423dc42c834efd197416c59942023385299
UncleYi <yizhaopu2008@163.com>
2018-04-10 20:32:03 +0800

    k

---
+++ project/core_transmit.py
@@ -0,0 +1,25 @@
+import socket
+
+logging.basicConfig(
+    format='[%(asctime)s] [%(levelname)s] [%(processName)s] [%(threadName)s] : %(message)s',
+    level=logging.INFO)
+
+MSG_LEN = 1024
+class core_transmit(object):
+
+    def transmit_thread(s_sock, d_sock):
+        try:
+            while True:
+                data = s_sock.recv(MSG_LEN)
+                d_sock.sendall(data)
+        except Exception as err:
+            logging.debug(err)
+
+    @staticmethod
+    def transmit_data(c_sock, p_sock, s_sock):
+        p_to_s = Thread(target=transmit_thread, args=(p_sock,s_sock,), daemon=True)
+        s_to_p = Thread(target=transmit_thread, args=(s_sock,p_sock,), daemon=True)
+        c_to_p = Thread(target=transmit_thread, args=(c_sock,p_sock,), daemon=True)
+        p_to_c = Thread(target=transmit_thread, args=(p_sock,c_sock,), daemon=True)
+        wt.start()
+        
---
+++ project/proxy.py
@@ -0,0 +1,18 @@
+import core_transmit
+import socket
+
+SERVER_PORT = 50001
+SERVER_ADDRESS = 'localhost'
+PROXY_ADDRESS = '0.0.0.0'
+PROXY_PORT = 60001
+BUFFER_SIZE = 2048
+
+if __name__ == '__main__':
+    proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    proxy_socket.bind((PROXY_ADDRESS, PROXY_PORT))
+    while True:
+        (client_socket, client_address) = proxy_socket.accept()
+        logging.debug('Accept client %s', client_address)
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.connect((SERVER_ADDRESS, SERVER_PORT))
+        core_transmit.transmit_data(client_socket, proxy_socket, server_socket)
