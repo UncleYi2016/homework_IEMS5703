471d84e64b3ecad71a3aa2d654cbdc0a9f06571f
UncleYi <yizhaopu2008@163.com>
2018-04-10 21:33:04 +0800

    sdf

--- project/core_transmit.py
+++ project/core_transmit.py
@@ -12,7 +12,6 @@ MSG_LEN = 1024
 def transmit_thread(s_sock, d_sock):
     try:
         while True:
-            logging.debug(s_sock)
             data = s_sock.recv(MSG_LEN)
             d_sock.sendall(data)
     except Exception as err:
@@ -24,10 +23,10 @@ def transmit_data(c_sock, p_sock, s_sock):
     logging.info(c_sock)
     logging.info(p_sock)
     logging.info(s_sock)
-    p_to_s = Thread(target=transmit_thread, args=(p_sock,s_sock,), daemon=True)
-    s_to_p = Thread(target=transmit_thread, args=(s_sock,p_sock,), daemon=True)
     c_to_p = Thread(target=transmit_thread, args=(c_sock,p_sock,), daemon=True)
     p_to_c = Thread(target=transmit_thread, args=(p_sock,c_sock,), daemon=True)
+    p_to_s = Thread(target=transmit_thread, args=(p_sock,s_sock,), daemon=True)
+    s_to_p = Thread(target=transmit_thread, args=(s_sock,p_sock,), daemon=True)
     p_to_s.start()
     s_to_p.start()
     c_to_p.start()
