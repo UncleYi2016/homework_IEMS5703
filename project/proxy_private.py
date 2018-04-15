c5b03ae4d3f4f4dfb9bafa19f209e6020d3644c6
UncleYi <yizhaopu2008@163.com>
2018-04-15 21:37:57 +0800

    dfg

--- project/proxy_private.py
+++ project/proxy_private.py
@@ -27,7 +27,7 @@ def connect():
         msg = core_transmit.get_operation(public_server_socket)
         logging.debug(msg)
         packet = json.loads(msg)
-        if packet.op_code == op_enum.OP_BUILD_CONNECTION:
+        if packet['op_code'] == op_enum.OP_BUILD_CONNECTION:
             tmp_public_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             tmp_public_socket.connect((PUBLIC_SERVER_ADDRESS, PUBLIC_SERVER_PORT))
             tmp_private_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
