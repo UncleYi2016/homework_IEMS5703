1ec8680c469aa994a80bfae239a320e8de29eabf
UncleYi <yizhaopu2008@163.com>
2018-04-15 21:33:50 +0800

    sdf

--- project/packet.py
+++ project/packet.py
@@ -2,7 +2,7 @@ import json
 
 def packet(op_code, op_describe, msg):
     packet_dict = {}
-    packet_dict.op_code = op_code
-    packet_dict.op_describe = op_describe
-    packet_dict.msg = msg
+    packet_dict['op_code'] = op_code
+    packet_dict.['op_describe'] = op_describe
+    packet_dict.['msg'] = msg
     return packet_dict
