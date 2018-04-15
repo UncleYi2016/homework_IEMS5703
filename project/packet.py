769fb10fcb75a46de68f3b6eaf5ca77c1c99fdfb
UncleYi <yizhaopu2008@163.com>
2018-04-15 21:30:00 +0800

    sdf

--- project/packet.py
+++ project/packet.py
@@ -1,14 +1,8 @@
 import json
 
-class packet(object):
-    op_code = 0
-    op_describe = ''
-    msg = ''
-
-    def packet(self, op_code, op_describe, msg):
-        self.op_code = op_code
-        self.op_describe = op_describe
-        self.msg = msg
-    
-    def to_json():
-        return json.dumps(self)
+def packet(op_code, op_describe, msg):
+    packet_dict = {}
+    packet_dict.op_code = op_code
+    packet_dict.op_describe = op_describe
+    packet_dict.msg = msg
+    return packet_dict
