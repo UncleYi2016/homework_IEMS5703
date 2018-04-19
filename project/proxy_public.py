4a4caa5545018b4402d65b5a4adf8d41797d6786
UncleYi <yizhaopu2008@163.com>
2018-04-19 19:25:25 +0800

    sdf

--- project/proxy_public.py
+++ project/proxy_public.py
@@ -134,6 +134,7 @@ def register_app(app_name=None, bind_port=None):
     BIND_APP[app_name] = bind_port
     try:
         (private_socket, private_address) = PROXY_SOCKET.accept()
+        private_socket.settimeout(5)
         logging.debug('accept address: ' + str(private_address))
         private_socket_element = {'app_name': app_name, 'private_socket': private_socket}
         PRIVATE_SOCKET_TABLE.append(private_socket_element)
@@ -165,6 +166,7 @@ if __name__ == '__main__':
     PROXY_SOCKET.listen(20)
     while True:
         (register_socket, register_address) = get_op_socket.accept()
+        register_socket.settimeout(5)
         get_register_thread = Thread(target=get_operation, args=(register_socket,), daemon=False, name='get_register_thread')
         get_register_thread.start()
     
