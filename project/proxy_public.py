efce775bbf860884397b22ba12730d02b78a609c
UncleYi <yizhaopu2008@163.com>
2018-04-22 20:46:06 +0800

    dsf

--- project/proxy_public.py
+++ project/proxy_public.py
@@ -106,7 +106,7 @@ def handle_operation():
                     PRIVATE_SOCKET_TABLE.remove(private_socket_element)
             for client_handle_element in CLIENT_HANDLE_TABLE:
                 if app_name == client_handle_element['app_name']:
-                    client_handle_socket = client_handle_element
+                    client_handle_socket = client_handle_element['client_handle_socket']
                     client_handle_socket.close()
                     CLIENT_HANDLE_TABLE.remove(client_handle_element)
 
