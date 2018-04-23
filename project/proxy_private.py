ce87c2b3b2d5b40ab1165da80fa7f83a20d50cc9
UncleYi <yizhaopu2008@163.com>
2018-04-23 23:06:13 +0800

    sdf

--- project/proxy_private.py
+++ project/proxy_private.py
@@ -27,7 +27,7 @@ PROXY_PORT = 8000
 
 logging.basicConfig(
     format='[%(asctime)s] [%(levelname)s] [%(processName)s] [%(threadName)s] : %(message)s',
-    level=logging.INFO)
+    level=logging.DEBUG)
 app = Flask(__name__)
 
 
